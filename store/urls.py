a8d5c293617ef13e8f5ded718ba31222fecb1f3e
KevinMisati <kevinmosigisi2001@gmail.com>
2024-10-10 13:57:32 +0300

    created a multicreate API for brands

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from .views import BrandViewSet,LaptopViewSet
 
 router = DefaultRouter()
 
-router.register(r'brands',BrandViewSet,basename='brand')
+router.register(r'brands',BrandViewSet.as_view(),basename='brands')
 router.register(r'laptops',BrandViewSet,basename='laptop')
 
 urlpatterns = router.urls
--- store/views.py
+++ store/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,generics,status
+from rest_framework.response import Response
 from .models import Brand,Laptop
 from .serializers import BrandSerializer,LaptopSerializer
 
 # Create your views here.
 
-class BrandViewSet(viewsets.ModelViewSet):
+class BrandViewSet(generics.ListCreateAPIView):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+
+    def create(self, request, *args, **kwargs):
+        if isinstance(request.data,list):
+            serializer = self.get_serializer(data=request.data,many=True)
+        else:
+            serializer = self.get_serializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED,headers=headers)
 
 class LaptopViewSet(viewsets.ModelViewSet):
     queryset = Laptop.objects.all()
