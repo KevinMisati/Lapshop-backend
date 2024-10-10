af73d448942966dd736f5080544baf010b937a3d
KevinMisati <kevinmosigisi2001@gmail.com>
2024-10-10 06:29:29 +0300

    testing the git credentials

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
     path('token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
     path('register/',register_user,name='register_user'),
-    path('logout/',register_user,name='logout'),
+    path('logout/',register_user,name='logout')
+    
 ]
 
 
