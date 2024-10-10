99e96ca9204a16781c3cf64d2e2dbd53062d0cba
KevinMisati <kevinmosigisi2001@gmail.com>
2024-10-10 12:58:05 +0300

    made the APIs public

--- lapshop/settings.py
+++ lapshop/settings.py
@@ -137,11 +137,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
 }
 
