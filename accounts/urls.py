4f1be996e46b6734297f0352216bc55f664965a3
KevinMisati <kevinmosigisi2001@gmail.com>
2024-10-15 05:47:47 +0300

    fixed an issue with the routing

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,13 +3,13 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import register_user, custom_login_view
+from .views import register_user, custom_login_view, logout_view
 
 urlpatterns = [
     path('token/',custom_login_view,name='token_obtain_pair'),
     path('token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
     path('register/',register_user,name='register_user'),
-    path('logout/',register_user,name='logout')
+    path('logout/',logout_view,name='logout')
 ]
 
 
